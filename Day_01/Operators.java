721811fdee1ed7d9f01963f732eeb9e71d97273c
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2023-11-26 21:32:20 +0530

    Create Operators.java

---
+++ Day_01/Operators.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Operators {
+    public static void main(String rgs[]){
+        int A = 10;
+        int B = 5;
+        System.out.println("add = "+(A+B));
+        System.out.println("Subtract = "+(A-B));
+        System.out.println("multiply ="+(A*B));
+        System.out.println("divide = "+(A/B));
+    }
+}
