10ea90456ab4b1c099436de18d929cbb007da747
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2023-11-26 21:48:01 +0530

    Create ProductInput.java

---
+++ Day_01/ProductInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+//product of two number
+public class ProductInput {
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int product = a*b;
+      System.out.println(product);  
+    }
+    
+}
