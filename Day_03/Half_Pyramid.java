bb85fcd84212b8272a3fd5f97154b747b1f62333
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2023-11-28 01:45:31 +0530

    Create Half_Pyramid.java

---
+++ Day_03/Half_Pyramid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class halfpyramid {
+    public static void main(String argas[]){
+        int n =4;
+
+        for(int line=1; line<=n; line++){
+            //number print
+            for(int number=1; number<=line; number++){
+                System.out.print( number);
+            }
+            System.out.println();
+        }
+
+    }
