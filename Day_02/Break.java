aaa68ef25865fbefc7a255b7592e8f334c95855f
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2023-12-05 23:19:37 +0530

    Update Break.java

--- Day_02/Break.java
+++ Day_02/Break.java
@@ -1,6 +1,6 @@
 import java.util.*;
     //break statement
-    public class breakk {
+    public class breakk{
         public static void main(String args[]){
             for(int i=1;i<=5;i++){
                 if(i == 3){//after i=3 the we will exit the loop
