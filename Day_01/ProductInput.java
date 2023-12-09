0566608908ee88d3cc5f86543f491768bb20341d
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2023-12-09 23:07:12 +0530

    Update ProductInput.java

--- Day_01/ProductInput.java
+++ Day_01/ProductInput.java
@@ -1,6 +1,6 @@
 import java.util.*;
 //product of two number
-public class ProductInput {
+public class ProductInput{
     public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
       int a = sc.nextInt();
