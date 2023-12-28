0e518d8ef200663652ba12bb30ccfb589a06c534
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2023-12-28 23:13:35 +0530

    Update Leetcode_Contains_Duplicate.java

--- Day_33/Leetcode_Contains_Duplicate.java
+++ Day_33/Leetcode_Contains_Duplicate.java
@@ -1,14 +1,13 @@
 https://leetcode.com/problems/contains-duplicate/
 
-class Solution {
+class Solution { 
     public boolean containsDuplicate(int[] nums) {
-        for(int i=0; i<nums.length;i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]== nums[j]){
-                    return true;
-                }
-            }
-        }
-        return false;
+            Arrays.sort(nums);//first sort the array
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == nums[i+1]) {
+				return true;
+			}
+		}
+		return false;
     }
 }
