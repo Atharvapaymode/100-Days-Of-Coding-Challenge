90c20405091c25bebb98e76e33eaaa3329c2c5fa
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-01-06 23:44:19 +0530

    Create Leetcode_concatenation-of-array.java

---
+++ Day_42/Leetcode_concatenation-of-array.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/concatenation-of-array/
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        final int n = nums.length;
+
+    int[] ans = new int[n * 2];
+
+    for (int i = 0; i < n; ++i)
+      ans[i] = ans[i + n] = nums[i];
+
+    return ans;
+    }
+}
