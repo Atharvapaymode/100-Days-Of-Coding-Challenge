31e2166186b2807dc5b1843568c76e20ca2e53d3
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-01-07 20:21:30 +0530

    Create Leetcode_Add_Digits.java

---
+++ Day_43/Leetcode_Add_Digits.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/add-digits/
+class Solution {
+    public int addDigits(int num) {
+         while(num > 9){
+            int res =0;
+            while(num > 0){
+                res += num%10;
+                num /= 10;
+            }
+            num = res;
+        }
+
+        return num;
+    }
+}
