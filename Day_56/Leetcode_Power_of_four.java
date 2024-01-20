3c78777b7bff55f17ca8451f30dde59f74cf8529
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-01-20 23:40:29 +0530

    Create Leetcode_Power_of_four.java

---
+++ Day_56/Leetcode_Power_of_four.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/power-of-four/
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        while (num > 3) {
+            if (num % 4 != 0) {
+                return false;
+            }
+            num = num / 4;
+        }
+        return num == 1;
+    }
+}
