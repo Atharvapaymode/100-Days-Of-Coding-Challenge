55094a7d9374a1980eeb470c3e683cbd79f9101b
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-01-26 21:44:09 +0530

    Create Best_Time_To_Buy_Sell_Stock.java

---
+++ Day_62/Best_Time_To_Buy_Sell_Stock.java
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+for (int i = 0; i < prices.length; i++) {
+for (int j = i+1; j <prices.length; j++) {
+maxProfit = Math.max(maxProfit,prices[j]-prices[i]);
+}
+}
+return maxProfit;
+    }
+}
