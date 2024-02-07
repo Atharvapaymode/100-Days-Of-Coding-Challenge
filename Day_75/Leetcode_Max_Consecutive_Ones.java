d3c06f30ef56f9c81ffc1353905bd14f079c8bc5
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-02-07 21:53:01 +0530

    Create Leetcode_Max_Consecutive_Ones.java

---
+++ Day_75/Leetcode_Max_Consecutive_Ones.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/max-consecutive-ones/description/
+class Solution {
+    public int findMaxConsecutiveOnes(int[] arr) {
+        int count = 0;
+        int ans = 0;
+        int n = arr.length;
+
+        for(int i=0; i<n; i++){
+            if(arr[i]==1) count++;
+            else{
+                ans = Math.max(ans,count);
+                count = 0;
+            }
+        }
+        return Math.max(ans,count);
+    }
+}
